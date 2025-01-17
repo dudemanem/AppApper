455b06af088b9b7ac9c7fbdb42f5faba1f9d1a3e
dudemanem <73352656+dudemanem@users.noreply.github.com>
2025-01-17 09:41:32 -0800

    HOLY SHIT THE GRID SYSTEM IS CONFUSING
    I tried to reconfigure the layout to use the grid system but it is not working 0-o. I'll try to fix it in a later commit..

--- AppApper_src/window_manager.py
+++ AppApper_src/window_manager.py
@@ -7,28 +7,30 @@ import saveload as data_manager
 
 #sets up root and grid system
 root = tk.Tk()
-root.columnconfigure(index=0, weight=1)
-root.rowconfigure(index=1, weight=1)
+root.columnconfigure(0, weight=0)
+root.rowconfigure(0, weight=0)
+
 
 #These are the default widgets of the app
 #|
 #\/
 
 #toolbar - currently a placeholder for a toolbar
-toolbar_container = tk.Frame(root, width=100, height=2)
-toolbar = tk.Label(toolbar_container,width=100, height=2, text="Toolbar", bg="green")
+toolbar = tk.Label(root,width=10, height=2, text="Toolbar", bg="green").grid(column=0,row=0,sticky=tk.NW)
 
 #shortcut box
-shortcut_container = tk.Frame(width=100,height=100,bg="red")
-shortcut_bg = tk.Label(shortcut_container, width=100,height=100,bg="red")
+#shortcut_container = tk.Frame(width=100,height=100,bg="red")
+shortcut_bg = tk.Label(root, width=20,height=20,bg="red").grid(column=0,row=1,sticky=tk.NW)
 
 
 
 #creates currently loaded shortcuts - I don't yet have a system for loading shortcuts finished
 def create_loaded_shortcuts():
+    row = 1
     for selected in data_manager.loaded_shortcuts:
-        sc = tk.Label(shortcut_container, width=selected.width, height=selected.heigth,bg="blue",text=selected.name)
-        sc.pack()
+        sc = tk.Label(shortcut_bg, width=selected.width, height=selected.heigth,bg="blue",text=selected.name).grid(column=0,row=row,sticky=tk.NW)
+        row += 1
+        
 
 
 #placeholder function for loading short
@@ -38,18 +40,17 @@ def load_shortcuts():
 
 #creates window and assigns the base window to root
 def innitialize_window():
-    global root,toolbar,shortcut_container,test
+    global root,toolbar
     root.title(app_display_name + " - " + app_version)
     #App Icon
     root.iconbitmap(r'AppApper_src\AppApper.ico')
     root.geometry(default_window_scale)
 
     #pack default elements
-    toolbar_container.pack(expand=True,fill=tk.X)
-    toolbar.pack(expand=True,fill=tk.X)
+    #toolbar_container.pack(expand=True,fill=tk.X)
+    
     create_loaded_shortcuts()
-    shortcut_bg.pack(expand=True,fill=tk.BOTH)
-    shortcut_container.pack(expand=True,fill=tk.BOTH)
+    
 
     #load data
     data_manager.load_metadata()
