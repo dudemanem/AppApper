9c4014a9bb9092f3ef1f23577da11dc75b7097ef
dudemanem <73352656+dudemanem@users.noreply.github.com>
2025-01-17 09:58:56 -0800

    random stuff idk

--- AppApper_src/window_manager.py
+++ AppApper_src/window_manager.py
@@ -7,8 +7,7 @@ import saveload as data_manager
 
 #sets up root and grid system
 root = tk.Tk()
-root.columnconfigure(0, weight=0)
-root.rowconfigure(0, weight=0)
+
 
 
 #These are the default widgets of the app
@@ -16,19 +15,19 @@ root.rowconfigure(0, weight=0)
 #\/
 
 #toolbar - currently a placeholder for a toolbar
-toolbar = tk.Label(root,width=10, height=2, text="Toolbar", bg="green").grid(column=0,row=0,sticky=tk.NW)
+toolbar_container = tk.Frame(root, width=100,height=2)
+toolbar = tk.Label(toolbar_container,width=10, height=2, text="Toolbar", bg="green")
 
 #shortcut box
-#shortcut_container = tk.Frame(width=100,height=100,bg="red")
-shortcut_bg = tk.Label(root, width=20,height=20,bg="red").grid(column=0,row=1,sticky=tk.NW)
-
+shortcut_container = tk.Frame(width=100,height=100,bg="red")
+shortcut_bg = tk.Label(shortcut_container, width=20,height=20,bg="red").pack(side="left")
 
 
 #creates currently loaded shortcuts - I don't yet have a system for loading shortcuts finished
 def create_loaded_shortcuts():
     row = 1
     for selected in data_manager.loaded_shortcuts:
-        sc = tk.Label(shortcut_bg, width=selected.width, height=selected.heigth,bg="blue",text=selected.name).grid(column=0,row=row,sticky=tk.NW)
+        sc = tk.Label(shortcut_bg, width=selected.width, height=selected.heigth,bg="blue",text=selected.name).pack()
         row += 1
         
 
@@ -47,8 +46,9 @@ def innitialize_window():
     root.geometry(default_window_scale)
 
     #pack default elements
-    #toolbar_container.pack(expand=True,fill=tk.X)
-    
+    toolbar_container.grid(column=0,row=0)
+    toolbar.pack(side="left")
+    shortcut_container.grid(column=0,row=1)
     create_loaded_shortcuts()
     
 
