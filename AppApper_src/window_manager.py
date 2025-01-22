cbfb3a1d1703b15ee0d19dc7fd2308b2ca687b92
dudemanem <73352656+dudemanem@users.noreply.github.com>
2025-01-22 10:22:03 -0800

    HOLY CRAP FINALLY THE GRID WORKS
    I finally figured out how to use the grid. Turns out the column and row configure functions actually determine how many rows and columns exist... I recoded the UI to us the column system. I only have the toolbar so far.

--- AppApper_src/window_manager.py
+++ AppApper_src/window_manager.py
@@ -7,7 +7,10 @@ import saveload as data_manager
 
 #sets up root 
 root = tk.Tk()
-
+root.title(app_display_name + " - " + app_version)
+#App Icon
+root.iconbitmap(r'AppApper_src\AppApper.ico')
+root.geometry(default_window_scale)
 
 
 #These are the default widgets of the app
@@ -16,40 +19,37 @@ root = tk.Tk()
 
 
 #toolbar - currently a placeholder for a toolbar
-toolbar_container = tk.Frame(root, width=400,height=20,bg="green")
-create_button = tk.Button(toolbar_container, width=10, height=2, text="Click Me!",pady=0)
+toolbar_container = tk.Frame(root,bg="green")
+toolbar_container.grid(row=0, column=0, sticky="ew",columnspan=30)
+for i in range(30):
+    toolbar_container.grid_columnconfigure(i, weight=1)
 
-#shortcut box
-shortcut_container = tk.Frame(root, width=300,height=300,bg="red")
 
+create_button = tk.Button(toolbar_container, text="Click Me")
+extra_button = tk.Button(toolbar_container, text="Testing Button :3")
 
+def configure_grid():
+    for i in range(30):
+        root.grid_columnconfigure(i, weight=1)  
+    for i in range(60):
+        root.grid_rowconfigure(i, weight=1)
+        
 #creates currently loaded shortcuts - I don't yet have a system for loading shortcuts finished
 def create_loaded_shortcuts():
     row = 1
     for selected in data_manager.loaded_shortcuts:
-        sc = tk.Label(shortcut_container, width=selected.width, height=selected.heigth,bg="orange",text=selected.name).pack(side="left",fill="none",pady=10,padx=10)
-        row += 1
+        pass
         
-
-
-#placeholder function for loading short
-def load_shortcuts():
-    pass
 
 
 #creates window and assigns the base window to root
 def innitialize_window():
     global root,create_button
-    root.title(app_display_name + " - " + app_version)
-    #App Icon
-    root.iconbitmap(r'AppApper_src\AppApper.ico')
-    root.geometry(default_window_scale)
 
-    #pack default elements
-    toolbar_container.pack(side="top",fill="x",padx=0,pady=0)
-    shortcut_container.pack(side="top",fill="both",pady=10)
-    create_button.pack(side="left",fill="none",pady=10)
-    create_loaded_shortcuts()
+    configure_grid()
+    #add default elements
+    create_button.grid(row=0,column=0,sticky="ew",pady=1)
+    extra_button.grid(row=0,column=1,sticky="ew",pady=1)
     
 
     #load data
@@ -57,6 +57,3 @@ def innitialize_window():
     
 
     root.mainloop()
-    
-    
-
