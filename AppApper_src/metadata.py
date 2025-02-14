31de20def504f62c5189c768c1a6bf1c637b3aab
dudemanem <73352656+dudemanem@users.noreply.github.com>
2025-02-14 09:35:53 -0800

    1.1.12 beta: I forgot to update version number again *facepalm*

--- AppApper_src/metadata.py
+++ AppApper_src/metadata.py
@@ -2,7 +2,7 @@ from tkinter import PhotoImage
 import os
 
 #contains basic data about app
-app_version = "v1.1.11 - beta"
+app_version = "v1.1.12 - beta"
 app_display_name = "AppApper"
 default_window_scale = "1300x750"
 #stores a bunch of random characters that can be assigned to the end of each image name. This is so that the chance of two shortcuts sharing the same image, and therefore both relying on it not getting deleted, won't happen.
